71bf1bef08aa38a81ac10ed06e6adf059cf385cc
xiaobogaga <m18202787110@gmail.com>
2019-01-02 17:27:13 +0800

    pass binary search tree, splay tree and avl tree test

--- src/test/java/com/tomzhu/tree/BinarySearchTreeTest.java
+++ src/test/java/com/tomzhu/tree/BinarySearchTreeTest.java
@@ -37,25 +37,46 @@ public class BinarySearchTreeTest {
 
     @Test
     public void insert() throws Exception {
-        /**
-         inserting : -844718477  -844, 2018, -942, 1832, 164
-         inserting : 2018823543
-         inserting : -942839450
-         inserting : 1832127170
-         inserting : 164074544
-         trying : 2018823543
-         trying : -844718477
-         trying : 1832127170
-         */
         tree = new BinarySearchTree();
-        int[] arr1 = new int[] {-844, 2018, -942, 1832, 164};
-        int[] arr2 = new int[] {2018, -844, 1832};
-        for (int i = 0; i < 5; i++) tree.insert(arr1[i]);
-
-        for (int i = 0; i < 5 ; i++) {
-            assertTrue(tree.remove(arr2[i]));
-            assertFalse(tree.contains(arr2[i]));
+        java.util.TreeMap<Integer, Integer> maps = new java.util.TreeMap<Integer, Integer> ();
+        java.util.HashMap<Integer, Integer> hashMap = new java.util.HashMap<>();
+        Random rand = new Random(System.currentTimeMillis());
+        assertTrue(tree.isEmpty());
+        for (int i = 0; i < size;) {
+            int k = rand.nextInt();
+            if (maps.containsKey(k)) continue;
+            // System.out.println("inserting : " + k);
+            maps.put(k, k);
+            hashMap.put(k, k);
+            tree.insert(k);
+            assertEquals(tree.getMin(), maps.firstKey());
+            assertEquals(tree.getMax(), maps.lastKey());
+            if (i != 0 && i % 5 == 0) {
+                Object[] arr = hashMap.values().toArray();
+                int l = rand.nextInt();
+                if (l < 0) l = -l;
+                int removedK = (Integer) arr[l % arr.length];
+                hashMap.remove(removedK);
+                maps.remove(removedK);
+                assertTrue(tree.contains(removedK));
+                tree.remove(removedK);
+                assertFalse(tree.contains(removedK));
+                assertEquals(tree.getMin(), maps.firstKey());
+                assertEquals(tree.getMax(), maps.lastKey());
+            }
+            i ++;
         }
+        assertFalse(tree.isEmpty());
+
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) assertTrue(tree.contains(entry.getKey()));
+
+        // testing remove.
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            // System.out.println("trying : " + entry.getKey());
+            assertTrue(tree.remove(entry.getKey()));
+            assertFalse(tree.contains(entry.getKey()));
+        }
+        assertTrue(tree.isEmpty());
     }
 
     @Test
--- src/test/java/com/tomzhu/tree/SplayTreeTest.java
+++ src/test/java/com/tomzhu/tree/SplayTreeTest.java
@@ -70,6 +70,45 @@ public class SplayTreeTest {
 
     @Test
     public void insert() throws Exception {
+        tree = new SplayTree<>();
+        java.util.TreeMap<Integer, Integer> maps = new java.util.TreeMap<Integer, Integer> ();
+        java.util.HashMap<Integer, Integer> hashMap = new java.util.HashMap<>();
+        Random rand = new Random(System.currentTimeMillis());
+        assertTrue(tree.isEmpty());
+        for (int i = 0; i < size;) {
+            int k = rand.nextInt();
+            if (maps.containsKey(k)) continue;
+            // System.out.println("inserting " + k);
+            maps.put(k, k);
+            hashMap.put(k, k);
+            tree.insert(k);
+            assertEquals(tree.getMin(), maps.firstKey());
+            assertEquals(tree.getMax(), maps.lastKey());
+            if (i != 0 && i % 5 == 0) {
+                Object[] arr = hashMap.values().toArray();
+                int l = rand.nextInt();
+                if (l < 0) l = -l;
+                int removedK = (Integer) arr[l % arr.length];
+                hashMap.remove(removedK);
+                maps.remove(removedK);
+                assertTrue(tree.contains(removedK));
+                tree.remove(removedK);
+                assertFalse(tree.contains(removedK));
+                assertEquals(tree.getMin(), maps.firstKey());
+                assertEquals(tree.getMax(), maps.lastKey());
+            }
+            i ++;
+        }
+        assertFalse(tree.isEmpty());
+
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) assertTrue(tree.contains(entry.getKey()));
+
+        // testing remove.
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            assertTrue(tree.remove(entry.getKey()));
+            assertFalse(tree.contains(entry.getKey()));
+        }
+        assertTrue(tree.isEmpty());
     }
 
     @Test
